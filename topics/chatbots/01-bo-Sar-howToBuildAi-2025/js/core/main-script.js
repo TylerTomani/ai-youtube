7e973fc241d96117571ebb3035453bf9157f85ca
tt <tt@MacBookPro.lan>
2025-11-05 08:32:19 -0500

    main

--- topics/chatbots/01-bo-Sar-howToBuildAi-2025/js/core/main-script.js
+++ topics/chatbots/01-bo-Sar-howToBuildAi-2025/js/core/main-script.js
@@ -1,15 +1,16 @@
 // main-script.js
 import { letterFocus } from "../nav/letter-focus.js";
 import { sideBarNav } from "../nav/side-bar-nav.js";
-import { handleStepNav } from "../nav/step-nav.js";
+
 import { getFocusZone } from "../nav/get-focus-zone.js";
 import { initDropDowns, } from "../ui/drop-downs-sidebar-temp.js";
+
+// step-navigation
+import { handleStepNav } from "../nav/step-nav.js";
 // uis
 import { initToggleSidebar } from "../ui/toggle-side-bar.js";
 import { lastClickedSideBarLink,lastFocusedSideBarLink } from "../nav/side-bar-nav.js";
 import { mainContentNav } from "../nav/main-content-nav.js";
-
-
 
 // Initialize drop-downs
 addEventListener('DOMContentLoaded', (e) => {
@@ -24,6 +25,7 @@ addEventListener('DOMContentLoaded', (e) => {
     
     // Keydown listener for the whole page
     addEventListener('keydown', e => {
+        let key = e.key.toLowerCase()
         const focusZone = getFocusZone({ e });
         // Letter navigation works for all zones
         letterFocus({ e, focusZone });
@@ -39,6 +41,10 @@ addEventListener('DOMContentLoaded', (e) => {
             case 'mainTargetDiv':
                 // Any custom main content keyboard logic here
                 mainContentNav({e,focusZone})
+                // 
+                //** We initialize new steps with initStepNavigation(mainTargetDiv) in
+                // inject-content.js  and use in handleStepNav in step-nav.js
+                //  */
                 handleStepNav({e, focusZone})
                 break;
 
